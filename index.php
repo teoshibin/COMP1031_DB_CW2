022d17a21e64f97071e71a3b9ed6cf2e4bfe19ea
teoshibin <teoshibin@gmail.com>
2020-03-16 10:01:11 +0800

    login interfaces

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sakila</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <link rel="stylesheet" href="dist/assets/css/header.css">
+</head>
+<body>
+    <?php 
+        include_once('dist/header.html');
+    ?>
+    <h1>Sakila</h1>
+    <h2>sakila the film renting website</h2>
+    <a href="dist/login.php">login</a>
+    <p>currently login as guest</p>
+</body>
+</html>
