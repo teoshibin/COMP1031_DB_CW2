4d27df4538f173c2abc1b0a52279d1b36ef84d8a
teoshibin <teoshibin@gmail.com>
2020-03-16 13:47:15 +0800

    commting

--- dist/config.php
+++ dist/config.php
@@ -7,10 +7,11 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'login');
  
 /* Attempt to connect to MySQL database */
+// $link = mysqli_connect("hostname", "username", "password", "database");
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
 if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
+    die("ERROR: Could not connect. " . mysqli_connect_error());//print error return from .mysqli_connect_error
 }
 ?>
