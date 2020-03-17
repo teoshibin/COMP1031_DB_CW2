cb8ced100575dbee44f70299e5503954c14fcc63
teoshibin <teoshibin@gmail.com>
2020-03-18 01:16:23 +0800

    adding video background

--- index.php
+++ index.php
@@ -1,20 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sakila</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
-    <link rel="stylesheet" href="dist/assets/css/header.css">
-</head>
-<body>
-
-    <?php 
-        include_once('dist/header.html');
-    ?>
-
-    <h2>sakila the film renting website</h2>
+<?php 
+    require_once('dist/header.html');
+?>
     <a href="dist/login.php">login</a>
-    <p>currently login as guest</p>
+
+<!-- <?php
+    // if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){ //isset prevent user from editing url to get in the logged in page
+    // header("location: welcome.php");// redirect function in php
+    // exit;
+    // }
+?> -->
+
 </body>
 </html>
